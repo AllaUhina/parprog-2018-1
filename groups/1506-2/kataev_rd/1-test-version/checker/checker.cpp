1042aa195b3e838a43493106be6eba225004e67c
Kataev Roman <r.kataev.unn@gmail.com>
2018-03-16 15:01:55 +0300

    Checker update
    Added SSIM comparison metric and an if-statement to see whether test passed

--- groups/1506-2/kataev_rd/1-test-version/checker/checker.cpp
+++ groups/1506-2/kataev_rd/1-test-version/checker/checker.cpp
@@ -9,6 +9,51 @@
 using namespace cv;
 using namespace std;
 
+//функция метрики SSIM для сравнения изображений
+double SSIM(Mat source1, Mat source2)
+{
+	double C = 0.01;
+	double averageSource = 0, averageSource2 = 0;
+	double sigma1 = 0.0, sigma2 = 0.0, sigma12 = 0.0;
+
+	//вычисление среднего для 1-го изображения
+	for (int i = 0; i < source1.rows; ++i)
+		for (int j = 0; j <source1.cols; ++j)
+			averageSource += source1.at<uchar>(i, j);
+	averageSource /= (source1.rows * source1.cols);
+
+	//вычисление среднего для 2-го изображения
+	for (int i = 0; i < source2.rows; ++i)
+		for (int j = 0; j <source2.cols; ++j)
+			averageSource2 += source2.at<uchar>(i, j);
+	averageSource2 /= (source2.rows * source2.cols);
+
+	//вычисление дисперсий (сигма)
+	for (int i = 0; i < source1.rows; ++i)
+		for (int j = 0; j <source1.cols; ++j)
+		{
+			sigma1 += pow((double)(source1.at<uchar>(i, j) - averageSource), 2);
+			sigma2 += pow((double)(source2.at<uchar>(i, j) - averageSource2), 2);
+			sigma12 += (source1.at<uchar>(i, j) - averageSource)*(source2.at<uchar>(i, j) - averageSource2);
+		}
+
+	sigma1 /= (source1.rows * source1.cols - 1);
+	sigma1 = sqrt(sigma1);
+
+	sigma2 /= (source2.rows * source2.cols - 1);
+	sigma2 = sqrt(sigma2);
+
+	sigma12 /= (source1.rows * source1.cols - 1);
+
+	double l, c, s;
+
+	l = (2 * averageSource * averageSource2 + C) / (averageSource * averageSource + averageSource2 * averageSource2 + C);
+	c = (2 * sigma1 * sigma2 + C) / (sigma1 * sigma1 + sigma2 * sigma2 + C);
+	s = (sigma12 + C) / (sigma1 * sigma2 + C);
+
+	return l * c * s;
+}
+
 int main(int argc, char *argv[])
 {
 	Mat inFileRaw, inFileCustom;
@@ -24,31 +69,24 @@ int main(int argc, char *argv[])
 		testName = "../../Tests/test" + to_string(i) + ".bin";
 		ifstream testFile;
 		testFile.open(testName, ios::in | ios::binary);
-		//testFile.seekg(0, ios::end);
-		
+
 		testFile >> picName;
-		cout << picName << endl;
 		testFile >> sigma;
-		cout << sigma << endl;
 
 		inFileRaw = imread(picPathRaw + picName, CV_LOAD_IMAGE_COLOR);
 		inFileCustom = imread(picPathCustom + picName, CV_LOAD_IMAGE_COLOR);
 
 		Mat outFileCV(inFileCustom.size(), CV_8UC3);
-		
+
 		GaussianBlur(inFileRaw, outFileCV, Size(3, 3), sigma, 0, BORDER_DEFAULT);
 
-		//	float similarity = 0.0;
+		double similarity = SSIM(inFileCustom, outFileCV);
 
-		//	for (int i = 0; i < inFileCustom.rows; i++)
-		//	{
-		//		for (int j = 0; j < inFileCustom.cols; j++)
-		//		{
-		//			similarity += pow(inFileCustom.at<uchar>(i, j) - outFileCV.at<uchar>(i, j), 2);
-		//		}
-		//	}
-
-		//	return 1.0 - sqrt(similarity) / (inFileCustom.rows * inFileCustom.cols);
+		cout << "Now testing image: " << i << endl;
+		cout << "Sigma: " << sigma << endl << "Similarity: " << similarity << endl;
+		if (similarity >= 0.99)
+			cout << "PASS!" << endl << endl;
+		else cout << "FAIL!" << endl << endl;
 
 		imwrite(outputPathCV + picName, outFileCV);
 
