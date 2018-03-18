17cd23649c1e26b82950f99f5222111bb9a29000
AVK96 <36678029+AVK96@users.noreply.github.com>
2018-03-18 21:36:52 +0300

    solver cyrillic comments

--- groups/1506-2/kostjunin_av/1-test-version/solver/solver.cpp
+++ groups/1506-2/kostjunin_av/1-test-version/solver/solver.cpp
@@ -1,6 +1,9 @@
 // solver.cpp : Defines the entry point for the console application.
 //
 
+
+
+
 #include <cstdio> 
 #include <omp.h> 
 #include <iostream>
@@ -8,6 +11,8 @@
 #include <exception>
 #include <algorithm>
 
+//#include "MyQSort.h"
+//#include "Standard.h"
 
 void MyQSort(long double * arr, unsigned long long size);
 void StandardSort(long double* arr, unsigned long long size);
@@ -20,17 +25,17 @@ int main(int argc, char * argv[])
 		return argc;
 	}
 
-	//argv[1] - èìÿ òåñòà
-	//argv[2] - êîëè÷åñòâî ïîòîêîâ(íåîáÿçàòåëüíûé àðãóìåíò)
+	//argv[1] - имя теста
+	//argv[2] - количество потоков(необязательный аргумент)
 
-	std::string test_name = argv[1];//èìÿ òåñòà
+	std::string test_name = argv[1];//имя теста
 
 	int num_threads = 1;
 	if (argc > 2)
 		num_threads = atoi(argv[2]);
 
 
-	auto filein = freopen(argv[1], "rb", stdin);					//âõîäíûå äàííûå
+	auto filein = freopen(argv[1], "rb", stdin);					//входные данные
 	if (!filein)
 	{
 		std::cerr << "input file not found" << std::endl;
@@ -39,29 +44,29 @@ int main(int argc, char * argv[])
 
 
 	auto name = test_name + ".ans";
-	FILE * out = fopen(name.c_str(), "wb");		//âûõîäíûå äàííûå("îòâåò ó÷àñòíèêà") - ðåçóëüòàò ðàáîòû íàïèñàííîé ôóíêöèè ñîðòèðîâêè
+	FILE * out = fopen(name.c_str(), "wb");		//выходные данные("ответ участника") - результат работы написанной функции сортировки
 	auto name1 = test_name + ".txt";
-	FILE * answer = fopen(name1.c_str(), "wb");	//ïðàâèëüíûé îòâåò("îòâåò æþðè") - ðåçóëüòàò ðàáîòû ýòàëîííîé ôóíêöèè ñîðòèðîâêè
+	FILE * answer = fopen(name1.c_str(), "wb");	//правильный ответ("ответ жюри") - результат работы эталонной функции сортировки
 
-	unsigned long long N = 0;//ðàçìåð ìàññèâà
-	fread(&N, sizeof(N), 1, stdin);//ñ÷èòûâàåì ðàçìåð ìàññèâà
+	unsigned long long N = 0;//размер массива
+	fread(&N, sizeof(N), 1, stdin);//считываем размер массива
 	long double * arr = new long double[N];
-	fread(arr, sizeof(*arr), N, stdin);//ñ÷èòûâàåì ìàññèâ
+	fread(arr, sizeof(*arr), N, stdin);//считываем массив
 	fclose(stdin);
-	long double * arr1 = new long double[N];//ìàññèâ äëÿ ýòàëîííîé ôóíêöèè
+	long double * arr1 = new long double[N];//массив для эталонной функции
 	memcpy(arr1, arr, sizeof(*arr) * N);
 
 
-	omp_set_num_threads(num_threads);//êîëè÷åñòâî ïîòîêîâ
-									 //ðåøåíèå çàäà÷è ñîðòèðîâêè - ðåçóëüòàò çàïèñûâàåòñÿ â "âûõîäíûå äàííûå"
+	omp_set_num_threads(num_threads);//количество потоков
+									 //решение задачи сортировки - результат записывается в "выходные данные"
 	try
 	{
 		double time = omp_get_wtime();
 		MyQSort(arr, N);
 		time = omp_get_wtime() - time;
 
-		fwrite(&time, sizeof(time), 1, out);//çàïèñûâàåì âðåìÿ ðàáîòû ñîðòèðîâêè
-		fwrite(arr, sizeof(*arr), N, out);//çàïèñûâàåì ðåçóëüòàò ðàáîòû ñîðòèðîâêè
+		fwrite(&time, sizeof(time), 1, out);//записываем время работы сортировки
+		fwrite(arr, sizeof(*arr), N, out);//записываем результат работы сортировки
 
 		std::cerr << "time = " << time << " is sorted? - " << std::boolalpha << std::is_sorted(arr, arr + sizeof(arr) / sizeof(*arr)) << std::endl;
 	}
@@ -72,15 +77,15 @@ int main(int argc, char * argv[])
 	fclose(out);
 
 	omp_set_num_threads(1);
-	//ðåøåíèå çàäà÷è ñîðòèðîâêè - ðåçóëüòàò çàïèñûâàåòñÿ â "ïðàâèëüíûé îòâåò.txt"
+	//решение задачи сортировки - результат записывается в "правильный ответ.txt"
 	try
 	{
 		double time = omp_get_wtime();
 		StandardSort(arr1, N);
 		time = omp_get_wtime() - time;
 
-		fwrite(&time, sizeof(time), 1, answer);//çàïèñûâàåì âðåìÿ ðàáîòû ñîðòèðîâêè
-		fwrite(arr1, sizeof(*arr1), N, answer);//çàïèñûâàåì ðåçóëüòàò ðàáîòû ñîðòèðîâêè
+		fwrite(&time, sizeof(time), 1, answer);//записываем время работы сортировки
+		fwrite(arr1, sizeof(*arr1), N, answer);//записываем результат работы сортировки
 
 		std::cerr << "time = " << time << " is sorted? - " << std::boolalpha << std::is_sorted(arr1, arr1 + sizeof(arr1) / sizeof(*arr1)) << std::endl;
 	}
