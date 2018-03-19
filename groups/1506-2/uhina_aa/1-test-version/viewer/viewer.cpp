861ff7e087c065d68265187aecd003af8892c13d
AllaUhina <allauhina@gmail.com>
2018-03-19 14:16:08 +0300

    change viewer

--- groups/1506-2/uhina_aa/1-test-version/viewer/viewer.cpp
+++ groups/1506-2/uhina_aa/1-test-version/viewer/viewer.cpp
@@ -6,55 +6,41 @@
 #include <iostream>
 using namespace std;
 
-FILE *stream;
-
-FILE *stream1;
-
 int main()
 {
+	FILE *stream;
+	FILE *stream2;
+
 	const char* FName = "..\\typer\\matr.bin";
 	const char* FName1 = "matr.txt";
 	const char* FName2 = "..\\solver\\matr.out";
-	/*freopen_s(&stream,, "r", stdin);
-	freopen_s(&stream1,"matr.txt", "w", stdout);*/
+
+	fopen_s(&stream, FName, "rb");
+	fopen_s(&stream2, FName2, "rb");
+
 	int N;
 	double *A, *B, *C;
 
-	//read matrix A and matrix B
-	ifstream in(FName, ios::binary);
-	in.read((char*)&N, sizeof(N));
+	fread(&N, sizeof(N), 1, stream);
+
 	A = new double[N * N];
 	B = new double[N * N];
 	C = new double[N * N];
-	for (int i = 0; i < N*N; i++)
-	{
-		in.read((char*)&A[i], sizeof(A[i]));
-	}
-	
-	for (int i = 0; i < N*N; i++)
-	{
-		in.read((char*)&B[i], sizeof(B[i]));
-	}
 
-	in.close();
-
-	//read matrix C
-	ifstream in2(FName2, ios::binary);
-	for (int i = 0; i < N*N; i++)
-	{
-		in2.read((char*)&C[i], sizeof(C[i]));
-	}
-	in2.close();
+	fread(A, sizeof(*A), N * N, stream);
+	fread(B, sizeof(*B), N * N, stream);
+	fread(C, sizeof(*C), N * N, stream2);
 
 	ofstream out;
 	out.open(FName1);
-	out<<N<<endl;
+	out << N << endl;
+
 	out << endl;
 	out << "Matrix A:" << endl;
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < N; j++)
-			out << A[i*N + j]<<"  ";
+			out << A[i*N + j] << "  ";
 		out << endl;
 	}
 
@@ -76,5 +62,8 @@ int main()
 		out << endl;
 	}
 	out.close();
+
+	fclose(stream);
+	fclose(stream2);
 }
 
